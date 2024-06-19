a92eb4f381e7b8ed382049e39939f7d1b03fe8d1
Munir <munircsdev@gmail.com>
2024-06-19 07:25:14 +0300

    tailwind config merge conflict fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./contact.html", "./src/input.css"],
   theme: {
     extend: {},
   },
   plugins: [require("@tailwindcss/forms")],
-=======
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./index.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
->>>>>>> new-main
 }
