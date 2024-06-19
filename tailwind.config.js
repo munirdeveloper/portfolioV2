25965ddefbcca1ce70f62e3e43eaf378b6581450
Munir <munircsdev@gmail.com>
2024-06-19 18:02:47 +0300

    comp library installed + particles tweaks

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./contact.html", "./src/input.css"],
+  content: [
+    "./index.html",
+    "./contact.html",
+    "./src/input.css",
+    "node_modules/preline/dist/*.js",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/forms")],
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("daisyui"),
+    require("preline/plugin"),
+  ],
 }
